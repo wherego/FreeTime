32d83fe048a969c7f68708c983baae7bdc0d9c3f
zachaxy <zachaxy@163.com>
2017-03-26 11:23:46 +0800

    添加新闻头条的bean对象

---
+++ app/src/main/java/com/zx/freetime/bean/topnews/TopNewsItem.java
@@ -0,0 +1,94 @@
+package com.zx.freetime.bean.topnews;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by zhangxin on 2017/3/26 0026.
+ * <p>
+ * Description :
+ */
+
+public class TopNewsItem {
+    private String uniquekey;
+    private String title;
+    private String date;
+    private String category;
+    private String authorName;
+    private String url;
+    private String thumbnailPicS;
+    private String thumbnailPicS02;
+    private String thumbnailPicS03;
+
+    public String getUniquekey() {
+        return uniquekey;
+    }
+
+    public void setUniquekey(String uniquekey) {
+        this.uniquekey = uniquekey;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailPicS() {
+        return thumbnailPicS;
+    }
+
+    public void setThumbnailPicS(String thumbnailPicS) {
+        this.thumbnailPicS = thumbnailPicS;
+    }
+
+    public String getThumbnailPicS02() {
+        return thumbnailPicS02;
+    }
+
+    public void setThumbnailPicS02(String thumbnailPicS02) {
+        this.thumbnailPicS02 = thumbnailPicS02;
+    }
+
+    public String getThumbnailPicS03() {
+        return thumbnailPicS03;
+    }
+
+    public void setThumbnailPicS03(String thumbnailPicS03) {
+        this.thumbnailPicS03 = thumbnailPicS03;
+    }
+}
